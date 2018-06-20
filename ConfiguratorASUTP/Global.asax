a02e49951657005d8ee956abd1651262a46a036f
Alex <skripinalexey1987@gmail.com>
2018-06-20 22:22:49 +0500

    Добавьте файлы проекта.

---
+++ ConfiguratorASUTP/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ConfiguratorASUTP.MvcApplication" Language="C#" %>
---
+++ ConfiguratorASUTP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ConfiguratorASUTP.Startup))]
+namespace ConfiguratorASUTP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
